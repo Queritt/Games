1522a87c90d9504177a7f6beb503a59acf86475a
Queritt <42835693+Queritt@users.noreply.github.com>
2023-03-15 15:53:40 +0300

    Update Elden_Ring_AutoSaver.bat
    Fix backup the same files.

--- Elden Ring/AutoSaver/Elden_Ring_AutoSaver.bat
+++ Elden Ring/AutoSaver/Elden_Ring_AutoSaver.bat
@@ -1,9 +1,9 @@
 @echo off
 rem Elden Ring: AutoSaver
-rem 0.1
-rem 2022/12/15
+rem 0.12
+rem 2023/03/15
 
-set game_folder="E:\Program Files (x86)\ELDEN RING\Game"
+set game_folder="E:\Software\ELDEN.RING.Deluxe.Edition.Steam.Rip-InsaneRamZes\ELDEN RING\Game"
 set process_name=eldenring.exe
 set profile_folder="C:\Users\USER\AppData\Roaming\EldenRing\76561197960267366"
 set backup_folder="C:\Users\USER\AppData\Roaming\EldenRing\76561197960267366\backup"
@@ -26,7 +26,7 @@ if %answer%==1 (
 ) else if %answer%==2 (
     goto Continue
 ) else (
-	goto Continue
+    goto Continue
 )
 
 :RecoveryBackup
@@ -42,14 +42,14 @@ cd %game_folder% && start %process_name%
 :CheckTaskRunning
 tasklist /fi "imagename eq %process_name%" | find "%process_name%" > nul 
 IF %ERRORLEVEL% == 1 (
-	color B
-	echo.
-	echo Active process %process_name% not found.
-	timeout /t 10
-	cls
-	echo.
-	goto CheckTaskRunning
-	)
+    color B
+    echo.
+    echo Active process %process_name% not found.
+    timeout /t 10
+    cls
+    echo.
+    goto CheckTaskRunning
+    )
 
 color F
 set hrs=%time:~0,2%
@@ -62,11 +62,16 @@ echo.
 cls
 echo.
 
-copy /Y "%profile_folder%\ER0000.sl2" "%backup_folder%\ER0000-%datetime%.sl2"
+for /f %%i in ('forfiles /p %profile_folder% /m *.sl2 /c "cmd /c echo @ftime"') do @set "current_file=%%i"
+for /f %%i in ('dir %backup_folder% /b/a-d/od/t:c') do set "backup_file_name=%%i"
+for /f %%i in ('forfiles /p %backup_folder% /m %backup_file_name% /c "cmd /c echo @ftime"') do @set "backup_file=%%i"
+
+if not %current_file%==%backup_file% copy /Y "%profile_folder%\ER0000.sl2" "%backup_folder%\ER0000-%datetime%.sl2"
 
 rem Powershell.exe -ExecutionPolicy Unrestricted -Command "Get-ChildItem -path %backup_folder% | where {$_.Lastwritetime -lt (date).addminutes(-10)} | remove-item -Recurse -Force"
 
-Powershell.exe -ExecutionPolicy Unrestricted -Command "$files = Get-ChildItem -Path %backup_folder% | Sort-Object creationtime -Descending; $count = $files.count - 10; if ($files.Count -igt 10) {$DelFiles = $files | Select-Object -last $count; Remove-Item -Path $DelFiles.fullname}"
+Powershell.exe -ExecutionPolicy Unrestricted -Command "$files = Get-ChildItem -Path %backup_folder% | Sort-Object creationtime -Descending; $count = $files.count - 30; if ($files.Count -igt 30) {$DelFiles = $files | Select-Object -last $count; Remove-Item -Path $DelFiles.fullname}"
 
-timeout /t 180
+rem timeout /t 180
+timeout /t 60
 goto Main
